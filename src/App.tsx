cf2d404c3d1e12ff8e0c168176e4211ebb3b5d67
parkyejin <uiop5809@naver.com>
2023-09-10 15:57:58 +0900

    Feat: 디자인 시스템 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import { ThemeProvider } from 'styled-components';
 import Router from './Router';
 import GlobalStyle from './styles/GlobalStyle';
+import { theme } from './styles/theme';
 
 function App() {
   return (
     <>
-      <GlobalStyle />
-      <Router />
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Router />
+      </ThemeProvider>
     </>
   );
 }
