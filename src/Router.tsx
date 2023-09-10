585a061781c5926c9db951f9912e2af910258a74
parkyejin <uiop5809@naver.com>
2023-09-10 16:46:51 +0900

    Design: 고객 지원 컴포넌트 (#6)

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route } from 'react-router-dom';
 import { Routes } from 'react-router-dom';
 import MainPage from './pages/main/main';
+import ClientPage from './pages/client/client';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainPage />} />
+        <Route path="/client/support" element={<ClientPage />} />
       </Routes>
     </BrowserRouter>
   );
