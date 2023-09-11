886f744dc3be1ec3e74fec3e6711f1a201dd6da0
minseo-hong <mshong1007@gmail.com>
2023-09-11 22:12:28 +0900

    Fix: 전역 상태에 있는 데이터 로컬로 옮기기

--- src/components/blog/BlogList.tsx
+++ src/components/blog/BlogList.tsx
@@ -1,7 +1,5 @@
 import { Link } from 'react-router-dom';
-import { useRecoilValue } from 'recoil';
 import styled from 'styled-components';
-import { blogListState } from '../../states/blog';
 
 interface BlogItemProps {
   id: number;
@@ -10,6 +8,79 @@ interface BlogItemProps {
   imageUrl: string;
   date: string;
 }
+
+const blogListData = [
+  {
+    id: 1,
+    title: '하이카디 발주 프로세스 및 홀터장비 신고방법',
+    category: '정보공유',
+    imageUrl: 'blog/thumbnail.jpg',
+    date: '2022.11.21',
+  },
+  {
+    id: 2,
+    title: '하이카디 발주 프로세스 및 홀터장비 신고방법',
+    category: '정보공유',
+    imageUrl: 'blog/thumbnail.jpg',
+    date: '2022.11.21',
+  },
+  {
+    id: 3,
+    title: '하이카디 발주 프로세스 및 홀터장비 신고방법',
+    category: '정보공유',
+    imageUrl: 'blog/thumbnail.jpg',
+    date: '2022.11.21',
+  },
+  {
+    id: 4,
+    title: '하이카디 발주 프로세스 및 홀터장비 신고방법',
+    category: '정보공유',
+    imageUrl: 'blog/thumbnail.jpg',
+    date: '2022.11.21',
+  },
+  {
+    id: 5,
+    title: '하이카디 발주 프로세스 및 홀터장비 신고방법',
+    category: '정보공유',
+    imageUrl: 'blog/thumbnail.jpg',
+    date: '2022.11.21',
+  },
+  {
+    id: 6,
+    title: '하이카디 발주 프로세스 및 홀터장비 신고방법',
+    category: '정보공유',
+    imageUrl: 'blog/thumbnail.jpg',
+    date: '2022.11.21',
+  },
+  {
+    id: 7,
+    title: '하이카디 발주 프로세스 및 홀터장비 신고방법',
+    category: '정보공유',
+    imageUrl: 'blog/thumbnail.jpg',
+    date: '2022.11.21',
+  },
+  {
+    id: 8,
+    title: '하이카디 발주 프로세스 및 홀터장비 신고방법',
+    category: '정보공유',
+    imageUrl: 'blog/thumbnail.jpg',
+    date: '2022.11.21',
+  },
+  {
+    id: 9,
+    title: '하이카디 발주 프로세스 및 홀터장비 신고방법',
+    category: '정보공유',
+    imageUrl: 'blog/thumbnail.jpg',
+    date: '2022.11.21',
+  },
+  {
+    id: 10,
+    title: '하이카디 발주 프로세스 및 홀터장비 신고방법',
+    category: '정보공유',
+    imageUrl: 'blog/thumbnail.jpg',
+    date: '2022.11.21',
+  },
+];
 
 const BlogListBlock = styled.header`
   display: grid;
@@ -75,11 +146,9 @@ const BlogItem = ({ id, title, category, imageUrl, date }: BlogItemProps) => {
 };
 
 const BlogList = () => {
-  const blogList = useRecoilValue(blogListState);
-
   return (
     <BlogListBlock>
-      {blogList.map((blog) => (
+      {blogListData.map((blog) => (
         <BlogItem
           key={blog.id}
           id={blog.id}
--- src/components/product-detail/ProductDetail.tsx
+++ src/components/product-detail/ProductDetail.tsx
@@ -1,18 +1,82 @@
 import styled from 'styled-components';
 import { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
-import { useRecoilValue } from 'recoil';
 
 import { IoIosArrowForward, IoIosArrowDown } from 'react-icons/io';
 
 import Button from '../common/Button';
-import { productListState } from '../../states/products';
 import { product } from '../../interfaces/product';
 
 interface DropdownMenuItemProps {
   onClick: React.MouseEventHandler<HTMLLIElement>;
   children: React.ReactNode;
 }
+
+const productListData = [
+  {
+    id: 1,
+    name: '하이카디플러스 HiCardi+',
+    description: '편리하게 심전도 측정, 전송 및 분석. 더 나은 삶을 위한 스마트한 솔루션 HiCardi+',
+    price: 2046000,
+    category: '기기 본체',
+  },
+  {
+    id: 2,
+    name: '하이카디플러스 HiCardi+ H100',
+    description: '메모리 내장형 제품편리하게 심전도 측정, 전송 및 분석. 더 나은 삶을 위한 스마트한 솔루션HiCardi+ H100',
+    price: 1320000,
+    category: '기기 본체',
+  },
+  {
+    id: 3,
+    name: '하이카디 HiCardi',
+    description: '편리하게 심전도 측정, 전송 및 분석. 더 나은 삶을 위한 스마트한 솔루션 HiCardi+',
+    price: 2046000,
+    category: '기기 본체',
+  },
+  {
+    id: 4,
+    name: '부정맥 전문의 원격 판독 서비스',
+    description: '임상병리사와 부정맥 전문의가 부정맥 진단 및 레포트 발행을 대행',
+    price: 27500,
+    category: '추가 서비스',
+  },
+  {
+    id: 5,
+    name: '심전도 전극 리필, 대형 20매',
+    description: '하이카디플러스 H100, 하이카디플러스 전용 리필 전극',
+    price: 22000,
+    category: '추가 용품',
+  },
+  {
+    id: 6,
+    name: '심전도 전극 리필, 소형 20매',
+    description: '하이카디 전용 리필 전극',
+    price: 16500,
+    category: '추가 용품',
+  },
+  {
+    id: 7,
+    name: '노닌 펄스옥시미터 3150',
+    description: '하이카디 플러스와 연동이 가능한 손목밴드형 산소포화도 측정 장비',
+    price: 1540000,
+    category: '추가 용품',
+  },
+  {
+    id: 8,
+    name: '노닌 펄스옥시미터 3230',
+    description: '하이카디 플러스와 연동이 가능한 클립형 산소포화도 측정 장비',
+    price: 550000,
+    category: '추가 용품',
+  },
+  {
+    id: 9,
+    name: '갤럭시A13',
+    description: '하이카디 서비스에 필요한 스마트폰(미개통)',
+    price: 275000,
+    category: '추가 용품',
+  },
+];
 
 const ProductDetailBlock = styled.div`
   display: grid;
@@ -165,16 +229,15 @@ const ProductDetail = () => {
   const navigate = useNavigate();
   const { productId } = useParams();
   const [product, setProduct] = useState<product | null>(null);
-  const productList = useRecoilValue(productListState);
 
   useEffect(() => {
     if (productId === '') {
       navigate(-1);
     } else {
-      const index = productList.findIndex((product) => product.id === Number(productId));
-      setProduct(productList[index]);
+      const index = productListData.findIndex((product) => product.id === Number(productId));
+      setProduct(productListData[index]);
     }
-  }, [productList]);
+  }, []);
 
   return (
     <ProductDetailBlock>
