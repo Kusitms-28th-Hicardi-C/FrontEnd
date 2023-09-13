47e939b481d6c7fb839333e9d535718d82a83a30
minseo-hong <mshong1007@gmail.com>
2023-09-13 21:32:03 +0900

    Design: 메인 페이지 롤링 배너 구현 완료

--- src/components/main/Banner.tsx
+++ src/components/main/Banner.tsx
@@ -1,20 +1,82 @@
 import styled from 'styled-components';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import { useRef } from 'react';
 
-import banner1 from '../../assets/banners/banner1.png';
-
-const BannerBlock = styled.section`
-  width: 70%;
-  margin: 0 auto;
+const BannerBlock = styled.div`
+  position: relative;
+  width: 100%;
 
   img {
     width: 100%;
   }
 `;
 
+const SlidePrevArrow = styled.button`
+  position: absolute;
+  top: 50%;
+  left: 70px;
+  transform: translateY(-50%);
+  z-index: 5;
+
+  img {
+    width: 30px;
+  }
+`;
+
+const SlideNextArrow = styled.button`
+  position: absolute;
+  top: 50%;
+  right: 70px;
+  transform: translateY(-50%);
+  z-index: 5;
+
+  img {
+    width: 30px;
+  }
+`;
+
+const BannerItem = styled.div`
+  img {
+    width: 100%;
+  }
+`;
+
 const Banner = () => {
+  const slickRef = useRef<Slider>(null);
+
+  const slideToPrev = () => {
+    if (slickRef.current) {
+      slickRef.current.slickPrev();
+    }
+  };
+
+  const slideToNext = () => {
+    if (slickRef.current) {
+      slickRef.current.slickNext();
+    }
+  };
+
   return (
     <BannerBlock>
-      <img src={banner1} alt="banner 1" />
+      <SlidePrevArrow type="button" onClick={slideToPrev}>
+        <img src="/images/icons/banner-left-arrow.svg" alt="배너 왼쪽 화살표" />
+      </SlidePrevArrow>
+      <Slider ref={slickRef} dots={false} infinite={true} speed={300} slidesToShow={1} autoplay={true} arrows={false}>
+        <BannerItem>
+          <img src="/images/banners/banner1.png" alt="banner 1" />
+        </BannerItem>
+        <BannerItem>
+          <img src="/images/banners/banner2.png" alt="banner 2" />
+        </BannerItem>
+        <BannerItem>
+          <img src="/images/banners/banner1.png" alt="banner 3" />
+        </BannerItem>
+      </Slider>
+      <SlideNextArrow type="button" onClick={slideToNext}>
+        <img src="/images/icons/banner-right-arrow.svg" alt="배너 오른쪽 화살표" />
+      </SlideNextArrow>
     </BannerBlock>
   );
 };
