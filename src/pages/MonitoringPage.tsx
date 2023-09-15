3a56b5c6f4e72616e28b7e948766d3b3165f10f9
parkyejin <uiop5809@naver.com>
2023-09-16 01:51:07 +0900

    Design: 하이카디 해결 5가지 환자 모니터링 컴포넌트

--- src/pages/MonitoringPage.tsx
+++ src/pages/MonitoringPage.tsx
@@ -1,11 +1,13 @@
 import MonitoringHeaderImage from '../components/monitoring/MonitoringHeaderImage';
 import Patient from '../components/monitoring/Patient';
+import Solve from '../components/monitoring/Solve';
 
 const MonitoringPage = () => {
   return (
     <>
       <MonitoringHeaderImage />
       <Patient />
+      <Solve />
     </>
   );
 };
