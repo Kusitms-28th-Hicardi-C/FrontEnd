1f47eccce683cbe323e7573eefd9d6e294b5179b
parkyejin <uiop5809@naver.com>
2023-09-17 22:11:32 +0900

    Feat: 브랜드 필름 video

--- src/components/brand/BrandFilm.tsx
+++ src/components/brand/BrandFilm.tsx
@@ -6,23 +6,20 @@ const BrandFilmBlock = styled.div`
   margin: 0 auto 2rem auto;
   padding-bottom: 2rem;
 
-  img {
+  video {
     width: 100%;
     cursor: pointer;
   }
 `;
-
 const BrandFilm = () => {
   return (
     <BrandFilmBlock>
       <Title>브랜드 필름</Title>
-      <a
-        href="https://drive.google.com/file/d/1mZ9MU4UB9kR9Py-GEN1Wgmk4X4LIT3_f/view"
-        rel="noopener noreferrer"
-        target="_blank"
-      >
-        <img src="/images/brand/brandfilm.svg" alt="브랜드 필름" />
-      </a>
+      <iframe
+        src="https://drive.google.com/file/d/1mZ9MU4UB9kR9Py-GEN1Wgmk4X4LIT3_f/preview"
+        width="100%"
+        height="520rem"
+      ></iframe>
     </BrandFilmBlock>
   );
 };
