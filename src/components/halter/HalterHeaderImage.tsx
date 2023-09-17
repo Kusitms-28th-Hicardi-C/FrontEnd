6e9f07a58e848879137310cfd57150d8dd7371a0
minseo-hong <mshong1007@gmail.com>
2023-09-17 19:52:04 +0900

    Feat: 이미지 S3에서 가져오기

--- src/components/halter/HalterHeaderImage.tsx
+++ src/components/halter/HalterHeaderImage.tsx
@@ -1,7 +1,7 @@
 import HeaderImage from '../common/HeaderImage';
 
 const HalterHeaderImage = () => {
-  return <HeaderImage imageUrl="/images/halter/banner.svg" alt="banner" />;
+  return <HeaderImage imageUrl="https://kusitms-cp-bucket.s3.ap-northeast-2.amazonaws.com/holter.svg" alt="banner" />;
 };
 
 export default HalterHeaderImage;
--- src/components/reviews/ReviewsHeaderImage.tsx
+++ src/components/reviews/ReviewsHeaderImage.tsx
@@ -1,7 +1,9 @@
 import HeaderImage from '../common/HeaderImage';
 
 const ReviewsHeaderImage = () => {
-  return <HeaderImage imageUrl="/images/reviews/banner.svg" alt="banner" />;
+  return (
+    <HeaderImage imageUrl="https://kusitms-cp-bucket.s3.ap-northeast-2.amazonaws.com/customerreview.svg" alt="banner" />
+  );
 };
 
 export default ReviewsHeaderImage;
