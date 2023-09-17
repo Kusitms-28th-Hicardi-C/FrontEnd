0f927a87b5fae9030afb2f13379904b306f65cc8
parkyejin <uiop5809@naver.com>
2023-09-17 23:46:21 +0900

    Feat: 이용문의 일대일 modal

--- src/components/client/UsageInquiry.tsx
+++ src/components/client/UsageInquiry.tsx
@@ -1,7 +1,8 @@
 import styled from 'styled-components';
 import useObserver from '../../hooks/useObserver';
-import { ContainerAnimation } from '../../styles/animation';
+import { ContainerAnimation, TextAnimation } from '../../styles/animation';
 import { motion } from 'framer-motion';
+import { useState } from 'react';
 
 const Banner = styled.div`
   background: linear-gradient(to bottom, #01a4ff, #00c2ff);
@@ -26,6 +27,7 @@ const Box = styled(motion.div)`
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.25);
   margin-top: 2rem;
   border-radius: 1rem;
+  cursor: pointer;
 `;
 
 const Left = styled.div`
@@ -54,13 +56,55 @@ const SmallText = styled.div`
   font-weight: 300;
 `;
 
+const ModalBackground = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.6);
+  z-index: 999;
+`;
+
+const ModalMain = styled(motion.div)`
+  position: absolute;
+  display: flex;
+  width: 30rem;
+  height: 10rem;
+  background-color: white;
+  border-radius: 1rem;
+  flex-direction: column;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.25);
+  justify-content: center;
+  align-items: center;
+  font-size: 1.2rem;
+  positoin: relative;
+
+  img {
+    position: absolute;
+    top: 1.5rem;
+    right: 1.5rem;
+    width: 1rem;
+    height: 1rem;
+    cursor: pointer;
+  }
+`;
+
 const UsageInquiry = () => {
   const { ref, animation } = useObserver();
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const handleModalOpen = () => {
+    setModalOpen(!modalOpen);
+  };
 
   return (
     <Banner>
       <Title>이용 문의</Title>
-      <Box ref={ref} animate={animation} variants={ContainerAnimation}>
+      <Box onClick={handleModalOpen} ref={ref} animate={animation} variants={ContainerAnimation}>
         <Left>
           <Content>하이카디에 관해 </Content>
           <Content>궁금한 점이 있으신가요? </Content>
@@ -79,6 +123,14 @@ const UsageInquiry = () => {
           <SmallText>* 상담가능시간 : 평일 09:00 ~ 17:00</SmallText>
         </Right>
       </Box>
+      {modalOpen && (
+        <ModalBackground onClick={handleModalOpen}>
+          <ModalMain onClick={handleModalOpen} initial="hidden" animate="visible" variants={TextAnimation}>
+            <img src="/images/supports/close.svg" />
+            준비중입니다.
+          </ModalMain>
+        </ModalBackground>
+      )}
     </Banner>
   );
 };
